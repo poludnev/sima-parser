2e7aa26b3681a6a27d666689006795eb63fdd8a5
poludnev <poludnev.roman@gmail.com>
2021-10-07 19:56:27 +0300

    parser refactored

--- src/simaLandParser.js
+++ src/simaLandParser.js
@@ -1,6 +1,5 @@
 const jsdom = require('jsdom');
-const { convertPrice } = require('./utils.js');
-const { countries, brands, categories } = require('./data.js');
+const { convertPrice, getItemData } = require('./utils.js');
 
 const { JSDOM } = jsdom;
 
@@ -26,45 +25,7 @@ module.exports = (file1, hostName = 'www.sima-land.ru') => {
 
     const itemDataBlock = priceParentDiv.parentElement.nextElementSibling;
 
-    let itemData;
-
-    if (itemDataBlock === null) {
-      itemData = [{ main: {}, other: ['n/a'] }];
-    } else {
-      itemData = [...itemDataBlock.children]
-        .map((e) => e.textContent)
-        .reduce(
-          (acc, val) => {
-            switch (true) {
-              case val.includes('Арт.:'):
-                acc.main.art = Number(val.split(':')[1].trim().split(';')[0]);
-                break;
-              case countries.includes(val.toLowerCase()):
-                acc.main.country = val;
-                break;
-              case val.includes('В боксе'):
-                acc.main.boxPcs = Number(val.slice(-'В боксе'.length).split(' ')[0]);
-                break;
-              case brands.includes(val.toLowerCase()):
-                acc.main.brand = val;
-                break;
-              case categories.includes(val.toLowerCase()):
-                acc.main.category = val;
-                break;
-              case val.includes('см ×'):
-                acc.main.size = val;
-                break;
-              case !!val:
-                acc.other.push(val);
-                break;
-              default:
-                throw new Error('parse error');
-            }
-            return acc;
-          },
-          { main: {}, other: [] },
-        );
-    }
+    const itemData = getItemData(itemDataBlock);
 
     const priceDiscountValue = convertPrice(priceDiscount.textContent);
 
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 const fss = require('fs');
+const { countries, brands, categories } = require('./data.js');
 
 exports.convertPrice = (string) => Number(string.replace(/[^0-9.,]+/g, '').replace(',', '.'));
 
@@ -6,4 +7,42 @@ exports.readJSONtoItemsSync = (filePath) => {
   const data = fss.readFileSync(`a01-json/${filePath}`, 'utf-8');
   const parsedData = JSON.parse(data);
   return parsedData;
+};
+
+exports.getItemData = (itemDataBlock) => {
+  if (itemDataBlock === null) return [{ main: {}, other: ['n/a'] }];
+  const result = [...itemDataBlock.children]
+    .map((e) => e.textContent)
+    .reduce(
+      (acc, val) => {
+        switch (true) {
+          case val.includes('Арт.:'):
+            acc.main.art = Number(val.split(':')[1].trim().split(';')[0]);
+            break;
+          case countries.includes(val.toLowerCase()):
+            acc.main.country = val;
+            break;
+          case val.includes('В боксе'):
+            acc.main.boxPcs = Number(val.slice(-'В боксе'.length).split(' ')[0]);
+            break;
+          case brands.includes(val.toLowerCase()):
+            acc.main.brand = val;
+            break;
+          case categories.includes(val.toLowerCase()):
+            acc.main.category = val;
+            break;
+          case val.includes('см ×'):
+            acc.main.size = val;
+            break;
+          case !!val:
+            acc.other.push(val);
+            break;
+          default:
+            throw new Error('parse error');
+        }
+        return acc;
+      },
+      { main: {}, other: [] },
+    );
+  return result;
 };
